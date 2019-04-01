3ad458562f0e02d6eb48e09a1673c1cea3a908d0
ezequiassam <ezequiassam@hotmail.com>
2019-04-01 09:40:12 -0300

    interation in paths

--- source_code_sniffer.py
+++ source_code_sniffer.py
@@ -5,15 +5,15 @@ import re
 
 regex_method = r'(public|private|protected).*(static|void|\w*)(.*\(.*\).*)({$)'
 regex_class = r'(^.*class.*{?)'
-score = {"lines": 0, "class": 0, "methods": 0, 'qtd bad smells found': 0, 'qtd bad lines class': 0, 'qtd bad lines methods': 0}
+score = []
 line_methods_max = 50
 line_class_max = 100
 
-path = os.getcwd()
+path = os.getcwd() + '/Dataset'
 
 
 def start_sniffer(fname):
-    count = {"lines": 0, "class": 0, "methods": 0, 'qtd bad smells found': 0, 'qtd bad lines class': 0, 'qtd bad lines methods': 0}
+    count = _sdata()['data']
     lines = file_to_array(fname)
     count["lines"] = len(lines)
     is_class = False
@@ -33,9 +33,9 @@ def start_sniffer(fname):
             count['qtd bad lines methods'] += 1
         print(l)
     if count['qtd bad lines class'] >= line_class_max:
-        count['qtd bad smells found'] += 1    
-    if count['qtd bad lines methods'] >= line_methods_max:    
-        count['qtd bad smells found'] += 1 
+        count['qtd bad smells found'] += 1
+    if count['qtd bad lines methods'] >= line_methods_max:
+        count['qtd bad smells found'] += 1
     return count
 
 
@@ -67,28 +67,44 @@ def input_params_from_god():
         global line_class_max
         line_methods_max = int(raw_input('Insira o número de linhas de código para um metódo ser considerado "deus":'))
         line_class_max = int(raw_input('Insira o número de linhas de código para uma clase ser considerado "deus":'))
-    except ValueError:       
-       print("Você digitou um valor que não é numero!")   
+    except ValueError:
+        print("Você digitou um valor que não é numero!")
+
+
+def _sdata():
+    return {"path": "", 'data': {"lines": 0, "class": 0, "methods": 0, 'qtd bad smells found': 0, 'qtd bad lines class': 0, 'qtd bad lines methods': 0}}
 
 
 def run():
-    input_params_from_god()
-    for filename in glob.glob(os.path.join(path, '*.java')):
-        print_console_format("ANALISE DO ARQUIVO")
-        print "Diretorio >>", filename, "<<\n\n"
-        count = start_sniffer(filename)
-        print_console_format("resultado")
-        for name, size in count.items():
-            print_console_result(name, size)
-            score[name] += size
-        print_console_format("+++++++++")
-        print "\n\n"
+    # input_params_from_god()
+    list_filenames = []
+    list_dirpaths = []
+    list_dirnames = []
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        list_filenames.extend(filenames)
+        list_dirnames.extend(dirnames)
+        list_dirpaths.extend(dirpath)
+        if filenames:
+            sdata = _sdata()
+            sdata['path'] = dirpath
+            for filename in glob.glob(os.path.join(dirpath, '*.java')):
+                print_console_format("ANALISE DO ARQUIVO")
+                print "Diretorio >>", filename, "<<\n\n"
+                count = start_sniffer(filename)
+                print_console_format("resultado")
+                for name, size in count.items():
+                    print_console_result(name, size)
+                    sdata['data'][name] += size
+                print_console_format("+++++++++")
+                print "\n\n"
+            score.append(sdata)
 
     print_console_format("resultado final")
-    for name, size in score.items():
-        print_console_result('Total of %s' % name, size)
-    print_console_result('Max lines methods', line_methods_max)    
-    print_console_result('Max lines class', line_class_max) 
+    for i in score:
+        for name, size in i:
+            print_console_result('Total of %s' % name, size)
+    print_console_result('Max lines methods', line_methods_max)
+    print_console_result('Max lines class', line_class_max)
     print_console_format("+++++++++++++++")
 
 
